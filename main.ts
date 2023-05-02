3b31dff2f50b20b3cc80e7fc25c416160536ba30
AyushR-Ranjan <122555382+AyushR-Ranjan@users.noreply.github.com>
2023-05-02 11:49:28 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -32,7 +32,7 @@ let action = ""
 let yValue = 0
 let xValue = 0
 comment.comment("CHANGE THE RADIO GROUP TO YOUR TEAM'S JOYSTICK NUMBER")
-radio.setGroup(27)
+radio.setGroup(61)
 basic.showIcon(IconNames.Yes)
 basic.showString("R")
 basic.forever(function () {
